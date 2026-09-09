3e85988637e09e17c9f830ad27fcb878ec5123d4
sss <sss@karma>
2026-09-09 16:49:02 -0400

    Suppress deprecation warnings.

--- k4ActsTracking/src/components/DD4hepBlueprintConstruction.cpp
+++ k4ActsTracking/src/components/DD4hepBlueprintConstruction.cpp
@@ -16,6 +16,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
 #include "DD4hepBlueprintConstruction.h"
 
 #include <Acts/Definitions/Algebra.hpp>
